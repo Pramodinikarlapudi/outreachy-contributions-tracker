3fdd191b53736e1253a7e21575e6e1aa18005bde
Pramodinikarlapudi <karlapudisol@gmail.com>
2025-04-02 06:12:58 +0530

    PCA plot Projections against Coconut

---
+++ OtherFeaturizersAnalysis/Visuals02.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.decomposition import PCA
+
+train = pd.read_csv("/mnt/d/outreachy-contributions-tracker/data/hERG_train.csv")
+valid = pd.read_csv("/mnt/d/outreachy-contributions-tracker/data/hERG_valid.csv")
+test = pd.read_csv("/mnt/d/outreachy-contributions-tracker/data/hERG_test.csv")
+
+data = pd.read_csv("/mnt/d/outreachy-contributions-tracker/data/hERG_coconut_features.csv")
+X = data.drop(columns=['SMILES', 'Label'])
+y = data['Label']
+
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+plt.scatter(X_pca[y == 0, 0], X_pca[y == 0, 1], c='skyblue', label='0 (Non-blockers)', alpha=0.5)
+plt.scatter(X_pca[y == 1, 0], X_pca[y == 1, 1], c='salmon', label='1 (Blockers)', alpha=0.5)
+plt.xlabel('PCA1')
+plt.ylabel('PCA2')
+plt.title('hERG PCA (eos8ub5 Features)')
+plt.legend()
+plt.savefig('/mnt/d/outreachy-contributions-tracker/figures/Coconut_hERG_pca_labels.png')
+plt.close()
